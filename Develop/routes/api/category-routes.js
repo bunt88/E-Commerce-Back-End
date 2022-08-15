27b16be75d71c769a9359147f465057815c548b6
unknown <bunt88@gmail.com>
2022-08-15 10:31:38 -0600

    Finished models

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -14,6 +14,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  
 });
 
 router.post('/', (req, res) => {
